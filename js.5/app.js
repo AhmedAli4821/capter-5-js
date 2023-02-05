e1f27074094307bebcd1bdb618559e1900dcbdef
Ahmed Ali <121734255+AhmedAli4821@users.noreply.github.com>
2023-02-05 10:26:52 -0800

    Chapter 5 & 6 To 11

---
+++ js.5/app.js
@@ -0,0 +1,404 @@
+document.write("<h3>Example Quise.# 1</h3>")
+
+document.write("<h3>Addition:</h3>")
+var a= (3)
+var b= (5)
+var c= a + b
+document.write("Sum of"+" "+ a +" " + "and"+" "+ b +" "+ "is"+ " " +c)
+
+document.write("<h3>Example Quise.#2 A</h3>")
+
+document.write("<h3>multiplication:</h3>")
+
+var d= (3);
+var e= (5);
+var f= d * e
+document.write("Multiply of" + " " + d + " " + "and" + " " + e + " " + "is" + " " +f)
+
+document.write("<h3>Example Quise.#2 B</h3>")
+
+document.write("<h3>division:</h3>")
+
+var ma= (5);
+var mb= (3);
+var mc= ma / mb
+document.write("Divide of" + " " + ma + " " +"and" + " " + mb + " " + "is" + " " + mc)
+
+document.write("<h3>Example Quise.#2 C</h3>")
+document.write("<h3>modulus:</h3>")
+
+var ba= (5);
+var bb= (3);
+var bc= ba % bb
+
+document.write("Divide of" + " " + ba +" "+ "and" +" "+ bb +" "+ "is" +" "+ bc)
+
+
+document.write('<h3> "Example Ques. # 03 A " </h3>')
+
+var num=
+document.write( "Value after variable declaration is" + " " + num)
+
+
+document.write("<h3>Example Ques. # 03 B</h3>")
+var num=(5);
+
+document.write("Initial Value:" +  num)
+
+document.write("<h3>Example Ques. # 03 C</h3>")
+
+var num=(5);
+
+document.write("Value after increment is:"+ " " + ++num)
+
+document.write("<h3>Example Ques. # 03 D</h3>")
+
+var num=(4);
+
+document.write("Value after increment is:"+ " " + ++num)
+
+document.write("<h3>Example Ques. # 03 E(a)</h3>")
+
+var numa=(5);
+var numb=(8);
+var numc= numa + numb
+
+document.write("Value after 2 Variable addition is:"+ " " + numc)
+
+document.write("<h3>Example Ques. # 03 E(b)</h3>")
+
+var num=(4);
+var numa=(5);
+var numc= num + numa
+
+document.write("Value after increment is:"+ " " + ++numc)
+
+document.write("<h3>Example Ques. # 03 F</h3>")
+
+var num=(6);
+var numa = num
+document.write("Value after increment is:" + " " + numa)
+
+document.write("<h3>Example Ques. # 03 G</h3>")
+
+var num = (6);
+var numa = num
+
+document.write("Value 6+1 after increment is:"+ " "+ ++numa)
+    
+document.write("<h3>Example Ques. # 03 H</h3>")
+
+var a =(6);
+var b =(6);
+var c = a+b
+
+document.write("Value after increment is 6+6="+" "+ ++c)
+
+document.write("<h3>Example Ques. # 03 I</h3>")
+
+var num = (6);
+var numa = -- num
+document.write("Value after increment is 6-1="+" "+ numa)
+
+document.write("<h3>Example Ques. # 03 J</h3>")
+
+var a = (19);
+var b = (6);
+var c = a-b
+document.write("Value After Decrement is 19-6="+ --c)
+
+document.write("<h3>Example Ques. # 03 K</h3>")
+var num =(10);
+var numa = num / 3
+document.write("After 10 dividing the variable's value by 3:"+ " " +"="+ numa)
+
+document.write("<h3>Example Ques. # 03 L</h3>")
+var a =(12);
+var b =(3);
+var c = a%b
+document.write("After 12 Modulus the variable's value by 3:"+ " " +"="+ " " +c)
+
+// Question No 04
+
+
+document.write('<h3> "Ques. # 04 " </h3>')
+var ticket= (600);
+var buying= (5);
+var total= ticket * buying
+document.write("Total cost to buy 5 tickets to a movie is"+ " " +total+ " " +"PKR")
+
+// Question No 05
+
+document.write('<h3> "Explain. # 05 " </h3>')
+document.write('<h3> "Table of 5" </h3>')
+
+var table =(5)
+var a = (1)
+var b = (2)
+var c = (3)
+var d = (4)
+var e = (5)
+var f = (6)
+var g = (7)
+var h = (8)
+var i = (9)
+var j = (10)
+document.write("5 x 1 =" +" "+ table * 1+"<br>")
+document.write("5 x 2 =" +" "+ table * 2+"<br>")
+document.write("1 x 5 =" +" "+ table * 3+"<br>")
+document.write("1 x 5 =" +" "+ table * 4+"<br>")
+document.write("1 x 5 =" +" "+ table * 5+"<br>")
+document.write("1 x 5 =" +" "+ table * 6+"<br>")
+document.write("1 x 5 =" +" "+ table * 7+"<br>")
+document.write("1 x 5 =" +" "+ table * 8+"<br>")
+document.write("1 x 5 =" +" "+ table * 9+"<br>")
+document.write("1 x 5 =" +" "+ table * 10+"<br>")
+
+// Question No 06
+document.write('<h3> "Explain. # 06 " </h3>')
+document.write("<h1>Temperature Converter</h1>")
+
+var c = 25;
+var f = (c*9 / 5)+ 32
+document.write("25<sup>0</sup>C"+" " +"is"+" " + f +"<sup>0</sup>F<br>")
+f = 70;
+c = (f-32)*5/9
+document.write("70<sup>0</sup>F"+" "+"is"+ c +"<sup>0</sup>C")
+
+// Question No 07
+document.write('<h3> "Explain. # 07 " </h3>')
+var item1 = 480;
+var item2 = 520;
+var q1 = 4;
+var q2 = 5;
+document.write("price of item 1 is:"+item1)
+document.write("<br>")
+document.write("Quantity of item 1 is"+q1)
+document.write("<br>")
+document.write("Price of item 2 is:"+ item2)
+document.write("<br>")
+document.write("Quantity of item 2 is"+q2)
+document.write("<br>")
+document.write("Shipping Charges: 100")
+document.write("<br>")
+document.write("Total Cost of Your Order is:"+(item1*4)+(item2*5)+100)
+
+
+// Question No 08
+document.write('<h3> "Explain. # 08 " </h3>')
+document.write("<h1>Marksheet</h1>")
+document.write("<br>")
+var obtained = 596;
+var totalp = 625;
+document.write("Total Marks : " + totalp)
+document.write("<br>")
+document.write("Obtained Marks : " + obtained)
+document.write("<br>")
+document.write((obtained / totalp) * 100)
+document.write("<br>")
+document.write("<br>")
+
+
+// Question No 09
+document.write('<h3> "Explain. # 09 " </h3>')
+
+document.write("<h1>Currency in PKR</h1>")
+var us = (104.80);
+var riyal = (28);
+var pkr = us * 10 + riyal * 25
+document.write("Total Currency in PKR:"+" "+ pkr)
+
+// Question No 10
+document.write('<h3> "Explain. # 10 " </h3>')
+
+var num = 5;
+num = (((num + 5) * 10)/2 );
+document.write("The Value is : " + num)
+
+// Question No 11
+document.write('<h3> "Explain. # 11 " </h3>')
+
+document.write("<h1>Age Calculator</h1>")
+
+var year =(2023);
+document.write("Current Year:"+ year)
+document.write("<br>")
+var year1 = (2000);
+document.write("Birth Year:"+ year1)
+document.write("<br>")
+var age = year - year1
+document.write("Your Age is:"+ age)
+
+// Question No 12
+document.write('<h3> "Explain. # 12 " </h3>')
+
+document.write("<h1>the Geometrizer</h1>")
+
+var radius = 20;
+var circumfrance;
+circumfrance = 2 * 3.142 * 20;
+var area;
+area = 3.142 * (20 * 20);
+document.write("The radius of the circle is : " + radius)
+document.write("<br>")
+document.write("The circumfrance of the circle is : " + circumfrance)
+document.write("<br>")
+document.write("The area of circle is : " + area)
+
+// Question No 13
+document.write('<h3> "Explain. # 13 " </h3>')
+
+var chai = "Chocolate Chip";
+var CurrentAge = 18;
+var MaxAge = 65;
+var max_snack = 3;
+
+document.write("Your Favourite Snack : " + chai)
+document.write("<br>")
+document.write("Your Current Age : " + CurrentAge)
+document.write("<br>")
+document.write("Estimated Maximum Age : " + MaxAge)
+document.write("<br>")
+document.write("Amount of Snacks Per Day : " + max_snack)
+document.write("<br>")
+var total_snack = ((MaxAge - CurrentAge) * (365 * max_snack))
+document.write("You will need " + total_snack + " to last you until the ripe old age of 65")
+
+
+// CHAPTER 6 //
+
+document.write("<h1>CHAPTER 6-9</h1>")
+
+document.write('<h3> "Explain. # Question 1 " </h3>')
+
+document.write("Result : ")
+document.write("<br>")
+document.write("The Value of a is : 10")
+document.write("<br>")
+document.write("_________________________")
+document.write("<br>")
+document.write("<br>")
+
+var a = 10;
+a++
+document.write("The value of ++a is : " + a);
+document.write("<br>")
+document.write("Now The Value of  a is : " + a)
+document.write("<br>")
+document.write("<br>")
+document.write("The Value of a++ is : " + a)
+document.write("<br>")
+a++
+document.write("Now the Value of a is : " + a)
+document.write("<br>")
+document.write("<br>")
+
+--a 
+document.write("The value of --a is : " + a)
+document.write("<br>")
+document.write("Now the value of a is : " + a)
+document.write("<br>")
+document.write("<br>")
+document.write("The value of a-- is : " + a)
+a--
+document.write("<br>")
+document.write("Now the value of a is : " + a)
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+
+document.write('<h3> "Explain. # Question 2 " </h3>')
+var b = 2;
+var c = 1;
+document.write("The Value of a is  " + b)
+document.write("<br>")
+document.write("The Value of b is " + c)
+document.write("<br>")
+document.write("<br>")
+
+--b;
+--b - --c;
+--b - --c + ++c;
+--b - --c + ++c + c--;
+document.write("Result is : " + b)
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+document.write('<h3> "Explain. # Question 3 " </h3>')
+var data = prompt("Enter Your Name"); 
+alert("Welcome "  + data);
+// document.write("<br>")
+// // document.write("<br>")
+
+
+document.write('<h3> "Explain. # Question 5 " </h3>')
+var t_data = prompt("Enter Number I'll show you it's table " , "5")
+document.write( t_data + " x 1 = " + t_data * 1)
+document.write("<br>")
+document.write( t_data + " x 2 = " + t_data * 2)
+document.write("<br>")
+document.write( t_data + " x 3 = " + t_data * 3)
+document.write("<br>")
+document.write( t_data + " x 4 = " + t_data * 4)
+document.write("<br>")
+document.write( t_data + " x 5 = " + t_data * 5)
+document.write("<br>")
+document.write( t_data + " x 6 = " + t_data * 6)
+document.write("<br>")
+document.write( t_data + " x 7 = " + t_data * 7)
+document.write("<br>")
+document.write( t_data + " x 8 = " + t_data * 8)
+document.write("<br>")
+document.write( t_data + " x 9 = " + t_data * 9)
+document.write("<br>")
+document.write( t_data +" x 10 = " + t_data * 10)
+document.write("<br>")
+document.write("<br>")
+
+document.write('<h3> "Explain. # Question 6 " </h3>')
+var sub1 = prompt("Subject 1","Urdu")
+var sub2 = prompt("Subject 2","English")
+var sub3 = prompt("Subgect 3","Maths")
+
+var t_marks = 100;
+sub1marks = prompt("Enter"+" "+ sub1+" "+ "Marks")
+sub2marks = prompt("Enter"+" "+ sub2 +" "+"Marks")
+sub3marks = prompt("Enter"+" "+sub3+" "+"Marks")
+
+document.write("<br>")
+var sub1marks;
+var sub2marks;
+var sub3marks;
+
+document.write("<h4>Subject 1</h4>")
+document.write("Subject : "+ sub1) 
+document.write("<br>")
+document.write("Total marks is : " + t_marks)
+document.write("<br>")
+document.write("Obtained Marks is : " + sub1marks)
+
+document.write("<h4>Subject 2</h4>")
+document.write("Subject : "+ sub2) 
+document.write("<br>")
+document.write("Total marks is : " + t_marks)
+document.write("<br>")
+document.write("Obtained Marks is : " + sub2marks)
+
+document.write("<h4>Subject 3</h4>")
+document.write("Subject : "+ sub3) 
+document.write("<br>")
+document.write("Total marks is : " + t_marks)
+document.write("<br>")
+document.write("Obtained Marks is : " + sub3marks)
+
+document.write("<h2>Percentage</h2>")
+var result = -(-sub1marks)-(-sub2marks)-(-sub3marks)
+document.write("Total Marks Obtained is : " + result)
+document.write("<br>")
+document.write("Total Marks : " + t_marks*3 )
+document.write("<br>")
+var per;
+per = (result*100) / 300;
+document.write("Your Percentage is :  " + per)
